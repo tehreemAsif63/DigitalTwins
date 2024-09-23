d2b9b6881aeb6c6c96e25db5d2e800860512628c
gusmojsma <gusmojsma@student.gu.se>
2024-09-23 16:34:29 +0200

    Add dependencies to server/client
    Client: Axios, Plotly, Typescript, Vite
    Server: Express.js, Nodemon

--- client/src/reportWebVitals.js
+++ client/src/reportWebVitals.js
@@ -1,6 +1,7 @@
-const reportWebVitals = onPerfEntry => {
+// Leave this file in place for now, but you can safely delete it in the future.
+const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
