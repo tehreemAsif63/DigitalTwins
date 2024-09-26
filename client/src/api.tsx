8f104f4d5480a8867b9ae431f710770bb227e424
Tehreem Asif <tehreem.asif63@gmail.com>
2024-09-27 00:23:09 +0200

    #8 Updates format styles, adds new title and several comments for clarity to enhance collaboration and save time
    Comments were added for clarity to enhance collaboration and save time

--- client/src/api.tsx
+++ client/src/api.tsx
@@ -1,5 +1,6 @@
+//axios to make  HTTP requests from a web browser
 import axios from "axios";
-
+//creates instance of axios with a predefined base URL
 export const Api = axios.create({
   baseURL: import.meta.env.VITE_API_ENDPOINT || "http://localhost:3000/api",
 });
