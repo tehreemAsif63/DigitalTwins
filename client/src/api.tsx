e4759e60237792d934db662904f041a437fdd334
Tehreem Asif <tehreem.asif63@gmail.com>
2024-09-29 16:57:24 +0200

    #7 Updates the base URL in api.tsx

--- client/src/api.tsx
+++ client/src/api.tsx
@@ -3,6 +3,6 @@ import axios from "axios";
 
 //creates instance of axios with a predefined base URL
 export const Api = axios.create({
-  baseURL: import.meta.env.VITE_API_ENDPOINT || "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_API_ENDPOINT || "http://localhost:3000/api/v1",
   headers: {'Content-Type': 'application/json',},
 });
