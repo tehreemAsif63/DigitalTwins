9bdd76d5769f9da4d56bbdfe26541a6eaa33520e
Omid Khodaparast <omidkhoda0101@gmail.com>
2024-10-03 18:33:25 +0200

    #18 Integrate data decryption with back-end and send the files to the front-end

---
+++ server/src/types/types.ts
@@ -0,0 +1,8 @@
+export interface PatientData {
+    data_qual_str : string[],
+    data_qual_time : number[],
+    measurement_data : number[],
+    start_date_time : string,
+    time_vector : number[],
+    units : string[]
+}
